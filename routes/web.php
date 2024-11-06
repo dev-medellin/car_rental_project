2cc8ae2c3af37ccf2f9b077e2415ea9ec55ecf7f
dev-arthur <devnull.arthur@gmail.com>
2024-11-07 02:03:44 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\CarCategory\CarCategoryController;
 use App\Http\Controllers\Admin\CarDetails\CarDetailsController;
+use App\Http\Controllers\Common\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,7 +10,7 @@ Route::get('/', function () {
 });
 
 Route::prefix('admin')->group(function () {
-    // Route::get('/dashboard', [DashboardController::class, 'index']);/
+    Route::get('/dashboard', [DashboardController::class, 'index']);
     Route::get('/car-category/list', [CarCategoryController::class, 'car_category'])->name('category.list-display');
     Route::get('/car-rental/list', [CarDetailsController::class, 'car_rental'])->name('category.list-display');
 });
